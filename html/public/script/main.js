197c111fb48f230a82b1430e8e9810fa186bca51
Tanyoung Kim <tanyoung.kim@nokia.com>
2015-04-19 22:50:15 -0700

    Updated footer

--- html/public/script/main.js
+++ html/public/script/main.js
@@ -33,16 +33,29 @@ require([
         if (p > 150) {
             $('.js-vis-axis').addClass('fixed');
             $('.js-header-title').fadeIn('fast');
+            $('.js-footer').fadeIn('fast');
         } else if (p > 50) {
             $('.js-vis-axis').removeClass('fixed');
             $('.js-header-title').fadeIn('fast');
+            $('.js-footer').fadeIn('fast');
         } else {
             $('.js-vis-axis').removeClass('fixed');
             $('.js-header-title').fadeOut('fast');
+            $('.js-footer').fadeOut('fast');
+            $('.js-source-content').hide();
         }
     }
+
     var scrolled = _.debounce(changeHeader, 100);
     $(window).scroll(scrolled);
+
+    //footer
+    $('.js-source').click(function() {
+        $('.js-source-content').toggle();
+    });
+    $('.js-close').click(function() {
+        $('.js-source-content').hide();
+    });
 
     //retreive data
     $.getJSON('dataset.json').done(function (d) {
